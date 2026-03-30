b00265b016cb2d1b43d44f76c312b9f1ec0dc9f2
MATHYSECE <picheret.m@gmail.com>
2026-03-30 09:01:11 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
     client teste = new client();
     menu.afficher(teste);
   }
--- src/visiteur.java
+++ src/visiteur.java
@@ -3,11 +3,12 @@ public class visiteur {
     public static void ecouter() {
         Scanner clavier = new Scanner(System.in);
         boolean stop = false;
-        int max = 0;
+        int max = 1;
         do {
             System.out.println("Quelle musique recherchez-vous ? Titre: ");
             String titre = clavier.nextLine();
                 musique.rechercher(titre);
+                max++;
         }while(stop == false && max < 5);
     }
 }
