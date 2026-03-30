38b1af39106f038cc2e9f1ccbc6d25c998046e7f
ThomasBuchler05 <thomas.buchler@edu.ece.fr>
2026-03-30 10:28:29 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     client teste = new client();
-    menu.afficher(teste);
-  }
+    while(true) {
+      menu.afficher(teste);
+    }  }
 }
